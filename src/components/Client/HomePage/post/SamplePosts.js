ede4b74b150af2c5276e4cd80db8d5e9c41f7449
“LeslieAine” <aineleslie@gmail.com>
2023-09-21 07:54:46 -0700

    sample posts used for boiler

---
+++ src/components/Client/HomePage/post/SamplePosts.js
@@ -0,0 +1,28 @@
+// samplePosts.js
+
+const samplePosts = [
+    {
+      id: 1,
+      user: {
+        username: 'user1',
+        avatar: 'user1-avatar.jpg',
+      },
+      content: 'This is the first sample post.',
+      likes: 10,
+      comments: 5,
+    },
+    {
+      id: 2,
+      user: {
+        username: 'user2',
+        avatar: 'user2-avatar.jpg',
+      },
+      content: 'Hello, Twitter!',
+      likes: 15,
+      comments: 7,
+    },
+    // Add more sample posts as needed
+  ];
+  
+  export default samplePosts;
+  
