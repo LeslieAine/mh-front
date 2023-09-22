6b79be28c0ba470efd41a4fc29f36e9dd5adf98e
“LeslieAine” <aineleslie@gmail.com>
2023-09-22 04:22:03 -0700

    Add Dm page

---
+++ src/components/creator/Pages/Messages/DmPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import DMList from '../../creator/Messages/DmList'
+
+function DmPage() {
+    const messages = [
+        {
+          user: {
+            username: 'user1',
+            avatar: 'user1-avatar.jpg',
+          },
+          message: 'Hello, how are you?',
+        },
+        {
+          user: {
+            username: 'user2',
+            avatar: 'user2-avatar.jpg',
+          },
+          message: 'I\'m doing great! Thanks for asking.',
+        },
+        // Add more DMs as needed
+      ];
+      
+  return (
+    <div>
+        <DMList messages= {messages} />
+    </div>
+  )
+}
+
+export default DmPage
