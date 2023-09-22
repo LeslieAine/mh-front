824fe2f7bbd0c50695da94b05e5845891b5644db
“LeslieAine” <aineleslie@gmail.com>
2023-09-22 04:13:37 -0700

    Add routes and navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
-import HomePage from './components/Client/HomePage/HomePage';
+import AppRouter from './routes/Router';
+import NavBar from './components/creator/NavBar/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
-}
+const App = () => (
+  <div className="app-container">
+    <BrowserRouter>
+      <section className="main-content">
+        <AppRouter />
+      </section>
+      <NavBar />
+    </BrowserRouter>
+  </div>
+);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <ClientHomePage /> */}
+//       <CreatorHomePage />
+//     </div>
+//   );
+// }
 
 export default App;
 library.add(fab, fas, far)
