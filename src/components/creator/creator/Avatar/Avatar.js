d31cf7ef0963e41e0401f468350c2b1e2f8202ad
LeslieAine <aineleslie@gmail.com>
2023-09-25 07:46:41 -0700

    Add link to the avatar

--- src/components/creator/creator/Avatar/Avatar.js
+++ src/components/creator/creator/Avatar/Avatar.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Avatar({ src, alt }) {
-  return <img src={src} alt={alt} className="avatar" />;
+  return (
+    <Link className="nav-link" to="/creator-profile/about-creator">
+      <img src={src} alt={alt} className="avatar" />
+    </Link>
+  );
 }
 
 export default Avatar;
