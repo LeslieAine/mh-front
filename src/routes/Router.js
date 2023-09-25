f52b02b884ea097798a0aacf27df31dc370f0b15
LeslieAine <aineleslie@gmail.com>
2023-09-25 07:46:27 -0700

    Update routes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -8,6 +8,11 @@ import CreatorHomePage from '../components/creator/Pages/CreatorHomePage/Creator
 import ContentPage from '../components/creator/Pages/Content/ContentPage';
 import OrderPage from '../components/creator/Pages/Orders/OrderPage';
 import DmPage from '../components/creator/Pages/Messages/DmPage';
+import ViewCreator from '../components/creator/Pages/ViewCreatorProfile/ViewCreatorProfilePage';
+import ContentList from '../components/creator/creator/content/ContentList';
+import About from '../components/creator/creator/About/About';
+import CreatorPostList from '../components/creator/creator/CreatorPosts/CreatorPostList';
+import samplePosts from '../components/Client/ClientHomePage/post/SamplePosts';
 
 
 const AppRouter = () => {
@@ -20,6 +25,35 @@ const AppRouter = () => {
 
 //     return children;
 //   };
+const contentData = [
+    {
+      title: 'Song Title 1',
+      numberOfBuys: 100,
+      price: '$0.99',
+      length: '3:45',
+    },
+    {
+      title: 'Song Title 2',
+      numberOfBuys: 85,
+      price: '$1.25',
+      length: '4:12',
+    },
+    {
+      title: 'Podcast Episode 1',
+      numberOfBuys: 50,
+      price: '$0.00 (Free)',
+      length: '25:30',
+    },
+    {
+      title: 'Ebook - Introduction to React',
+      numberOfBuys: 120,
+      price: '$9.99',
+      length: '150 pages',
+    },
+    // Add more content items as needed
+  ];
+
+
   return (
     <Routes>
       {/* <Route exact path="/" element={<LandingPage />} /> */}
@@ -47,6 +81,11 @@ const AppRouter = () => {
             <DmPage />
       )}
       />
+        <Route path="/creator-profile" element={<ViewCreator />}>
+            <Route path="about-creator" element={<About />} />
+            <Route path="content-list" element={<ContentList contentData={contentData} />} />
+            <Route path="posts" element={<CreatorPostList posts = {samplePosts}/>} />
+        </Route>
       <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
   );
