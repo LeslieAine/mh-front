75ab7fa9fea6eec0b0484cdf4ce33b94d5bbaa0b
LeslieAine <aineleslie@gmail.com>
2023-09-25 07:48:50 -0700

    Update the creator homepage

--- src/components/creator/Pages/CreatorHomePage/CreatorHomePage.js
+++ src/components/creator/Pages/CreatorHomePage/CreatorHomePage.js
@@ -2,14 +2,17 @@ import React from 'react'
 import Header from './header/Header'
 import PostList from './post/PostList'
 import samplePosts from './post/SamplePosts'
-import Footer from '../../NavBar/Footer/Footer'
+// import Footer from '../../NavBar/Footer/Footer'
+import ContentCreationBar from '../../creator/CreateContent/CreateContentBar'
+import ViewCreator from '../ViewCreatorProfile/ViewCreatorProfilePage'
 
 function CreatorHomePage() {
   return (
     <div>
-        <Header />
+        {/* <Header />
         <PostList posts={samplePosts} />
-        {/* <Footer /> */}
+        <ContentCreationBar /> */}
+        <ViewCreator />
     </div>
   )
 }
