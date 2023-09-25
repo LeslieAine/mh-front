6c0570e2229a808fea80f10c00a35892d8b2b776
LeslieAine <aineleslie@gmail.com>
2023-09-25 07:47:50 -0700

    Add post cards and post lists for a single creator

---
+++ src/components/creator/creator/CreatorPosts/CreatorPostCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactionsBar from './ReactionsBar/ReactionsBar'
+import './PostCard.css'
+
+function CreatorPostCard({post}) {
+  return (
+    <Link to={`/posts/${post.id}`} className="post-card">
+        <div className='postcard'>
+            <div className="user-info">
+                <img className='post-card-avatar' src={post.user.avatar} alt={`${post.user.username}'s Avatar`} />
+                <span className='post-card-username'>{post.user.username}</span>
+            </div>
+            <div className="content">
+                <p>{post.content}</p>
+            </div>
+            <ReactionsBar />
+        </div>
+    </Link>
+  )
+}
+
+export default CreatorPostCard
