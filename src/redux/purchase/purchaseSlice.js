2fb48fb6d485aad519613cf593d6ba3d263f4bcb
LeslieAine <aineleslie@gmail.com>
2023-09-27 09:05:00 -0700

    Add purchase reducer

---
+++ src/redux/purchase/purchaseSlice.js
@@ -0,0 +1,47 @@
+// purchaseSlice.js
+
+import { createSlice } from '@reduxjs/toolkit';
+import { updateBalance } from './userSlice'; // Import the userSlice action
+
+const initialState = {
+    purchases: [], // Array to store purchase history
+};
+
+const purchaseSlice = createSlice({
+  name: 'purchase',
+  initialState,
+  reducers: {
+    addPurchase: (state, action) => {
+      state.purchases.push(action.payload);
+    },
+  },
+});
+
+// Async action to handle a purchase
+export const purchaseContent = (contentId, userId, contentPrice) => async (dispatch, getState) => {
+  const userBalance = getState().user.balance; // Assuming you have user data in the state
+
+  if (userBalance >= contentPrice) {
+     // Calculate the updated balance after deduction
+     const updatedBalance = userBalance - contentPrice;
+
+     // Dispatch the updateBalance action to update the user's balance
+     dispatch(updateBalance(updatedBalance));
+
+    // Create a new purchase record
+    const newPurchase = {
+      contentId,
+      userId,
+      transactionDate: new Date(),
+    };
+
+    // Dispatch the addPurchase action to add the purchase to the history
+    dispatch(addPurchase(newPurchase));
+  } else {
+    // Handle insufficient balance (e.g., show an error message)
+  }
+};
+
+export const { addPurchase } = purchaseSlice.actions;
+
+export default purchaseSlice.reducer;
