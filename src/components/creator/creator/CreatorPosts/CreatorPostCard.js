12eb485dca74f8cf14407994e76bf6013b17804f
LeslieAine <aineleslie@gmail.com>
2023-09-27 09:04:08 -0700

    Update to accomodate content and post reducers

--- src/components/creator/creator/CreatorPosts/CreatorPostCard.js
+++ src/components/creator/creator/CreatorPosts/CreatorPostCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import ReactionsBar from './ReactionsBar/ReactionsBar'
 import './PostCard.css'
 
--- src/components/creator/creator/content/ContentCard.js
+++ src/components/creator/creator/content/ContentCard.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchContent } from './contentSlice';
 import './ContentCard.css';
 
 function ContentCard({ title, numberOfBuys, price, length }) {
+    const dispatch = useDispatch();
+
+    const content = useSelector((state) => state.content.content);
+    const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchContent(id));
+  }, [dispatch, id]);
+
   return (
     <div className="content-card">
       <div className="card-header">
