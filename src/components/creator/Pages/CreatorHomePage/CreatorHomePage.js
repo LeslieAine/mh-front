c3e824b161d112ebed80d4969ac09173ba293c66
LeslieAine <aineleslie@gmail.com>
2023-10-03 08:47:37 -0700

    Edit creator homepage

--- src/components/creator/Pages/CreatorHomePage/CreatorHomePage.js
+++ src/components/creator/Pages/CreatorHomePage/CreatorHomePage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+// import Header from './header/Header'
+// import CreatorHeader from '../../creator/CreatorHeader'
 import Header from './header/Header'
-import PostList from './post/PostList'
-import samplePosts from './post/SamplePosts'
+import { Outlet } from 'react-router-dom'
+// import PostList from './post/PostList'
+// import samplePosts from './post/SamplePosts'
 // import Footer from '../../NavBar/Footer/Footer'
-import ContentCreationBar from '../../creator/CreateContent/CreateContentBar'
-import ViewCreator from '../ViewCreatorProfile/ViewCreatorProfilePage'
+// import ContentCreationBar from '../../creator/CreateContent/CreateContentBar'
+// import ViewCreator from '../ViewCreatorProfile/ViewCreatorProfilePage'
 
 function CreatorHomePage() {
   return (
@@ -12,7 +15,13 @@ function CreatorHomePage() {
         {/* <Header />
         <PostList posts={samplePosts} />
         <ContentCreationBar /> */}
-        <ViewCreator />
+        {/* <ViewCreator /> */}
+        {/* <Link to="/creator-profile/about-creator">About</Link>
+        <Link to="/creator-profile/content-list">Content</Link>
+        <Link to="/creator-profile/posts">Posts</Link> */}
+        {/* <CreatorHeader /> */}
+        <Header />
+        <Outlet />
     </div>
   )
 }
