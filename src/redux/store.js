78f3d02490c3afad6812a0a291c079152e841ece
LeslieAine <aineleslie@gmail.com>
2023-10-03 08:47:03 -0700

    Add store to handle reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userSlice';
+import userReducer from './user/userSlice';
+import authReducer from './authentication/AuthenticationSlice';
+import contentReducer from './content/contentSlice'
+import favoritesReducer from './favorite/favoriteSlice'
+import postReducer from './posts/postSlice'
+import purchaseReducer from './purchase/purchaseSlice';
+import messagesReducer from './messages/messagesSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    authentication: authReducer,
+    content: contentReducer,
+    favorites: favoritesReducer,
+    post: postReducer,
+    purchase: purchaseReducer,
+    messages: messagesReducer
     // Add other reducers as needed
   },
 });
