631a2a096e19fa7f17b773b668a9c4c81507d183
LeslieAine <aineleslie@gmail.com>
2023-10-05 10:43:43 -0700

    Update end points for data

--- src/components/Client/ClientHomePage/post/PostCard.js
+++ src/components/Client/ClientHomePage/post/PostCard.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import ReactionsBar from './ReactionsBar/ReactionsBar'
-import './PostCard.css'
 
-function PostCard({post}) {
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom'
+import { fetchPostById } from '../../../../redux/posts/postSlice'; // Import the fetchPostById action
+
+import ReactionsBar from './ReactionsBar/ReactionsBar';
+import './PostCard.css';
+
+function PostCard({ postId }) {
+  const dispatch = useDispatch();
+  const post = useSelector((state) => state.posts.posts.find((p) => p.id === postId)); // Assuming your Redux state structure
+
+  useEffect(() => {
+    // Dispatch the fetchPostById action when the component mounts
+    dispatch(fetchPostById(postId));
+  }, [dispatch, postId]);
+
+  if (!post) {
+    // Handle the case where the post is not available yet (e.g., still loading)
+    return null;
+  }
+
   return (
-    <div className='postcard'>
-      <div className="user-info">
-        <img className='post-card-avatar' src={post.user.avatar} alt={`${post.user.username}'s Avatar`} />
-        <span className='post-card-username'>{post.user.username}</span>
+    <Link to={`/posts/${post.id}`} className="post-card">
+      <div className="postcard">
+        <div className="user-info">
+          <img className="post-card-avatar" src={post.user.avatar} alt={`${post.user.username}'s Avatar`} />
+          <span className="post-card-username">{post.user.username}</span>
+        </div>
+        <div className="content">
+          <p>{post.content}</p>
+        </div>
+        <ReactionsBar />
       </div>
-      <div className="content">
-        <p>{post.content}</p>
-      </div>
-      <ReactionsBar />
-    </div>
-  )
+    </Link>
+  );
 }
 
-export default PostCard
+export default PostCard;
