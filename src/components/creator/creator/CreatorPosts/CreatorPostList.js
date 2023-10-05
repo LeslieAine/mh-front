eb1b5dc77071b21a13797c9651d3c7c8945ee1d1
LeslieAine <aineleslie@gmail.com>
2023-10-05 10:44:37 -0700

    Edit api endpoints for cards

--- src/components/creator/creator/CreatorPosts/CreatorPostList.js
+++ src/components/creator/creator/CreatorPosts/CreatorPostList.js
@@ -1,22 +1,49 @@
-import React from 'react';
-// import PostCard from './PostCard';
+// import React from 'react';
+// // import PostCard from './PostCard';
+// import CreatorPostCard from './CreatorPostCard';
+
+// function CreatorPostList({ posts }) {
+//   return (
+//     <div className="post-list">
+//       {posts.map((post) => (
+//         <CreatorPostCard
+//           key={post.id}
+//           post = {post}
+//         //   user={post.user}
+//         //   content={post.content}
+//         //   likes={post.likes}
+//         //   comments={post.comments}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default CreatorPostList;
+
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../../../../redux/posts/postSlice'; // Import the fetchPosts action
+
 import CreatorPostCard from './CreatorPostCard';
 
-function CreatorPostList({ posts }) {
+function CreatorPostList() {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.posts); // Assuming your Redux state structure
+
+  useEffect(() => {
+    // Dispatch the fetchPosts action when the component mounts
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
   return (
     <div className="post-list">
       {posts.map((post) => (
-        <CreatorPostCard
-          key={post.id}
-          post = {post}
-        //   user={post.user}
-        //   content={post.content}
-        //   likes={post.likes}
-        //   comments={post.comments}
-        />
+        <CreatorPostCard key={post.id} post={post} />
       ))}
     </div>
   );
 }
 
 export default CreatorPostList;
+
