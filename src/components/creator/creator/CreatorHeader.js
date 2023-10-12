a8e14349ac769f6b40cf6c4dbd820d482957c05d
LeslieAine <aineleslie@gmail.com>
2023-10-12 10:59:13 -0700

    Add logout button to header

--- src/components/creator/creator/CreatorHeader.js
+++ src/components/creator/creator/CreatorHeader.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import './CreatorHeader.css'
 import Avatar from './Avatar/Avatar';
 import Points from './Points/Points';
 import Follow from './Follow/Follow';
 import FavoritedBy from './FavoritedBy/FavoritedBy';
+import { logoutUser } from '../../../redux/authentication/AuthenticationSlice';
 
 function CreatorHeader({ creator }) {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    localStorage.removeItem('user');
+    window.location.reload();
+  };
 
     return (
       <div className="creator">
@@ -13,6 +22,10 @@ function CreatorHeader({ creator }) {
         <Follow />
         <FavoritedBy />
         <Points points={creator.points} />
+        <button type="button" onClick={handleLogout} className="signout-btn">
+            {/* <FaUserCircle className="btn-logo" /> */}
+            Log Out
+          </button>
       </div>
     );
 }
