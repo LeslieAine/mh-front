68d048845b19cd7e1d61e90d41a817c7a347f269
LeslieAine <aineleslie@gmail.com>
2023-10-12 11:00:17 -0700

    Update the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';
+// import userReducer from './user/userSlice';
 import authReducer from './authentication/AuthenticationSlice';
 import contentReducer from './content/contentSlice'
 import favoritesReducer from './favorite/favoriteSlice'
+import postsReducer from './posts/postsSlice'
 import postReducer from './posts/postSlice'
 import purchaseReducer from './purchase/purchaseSlice';
 import messagesReducer from './messages/messagesSlice';
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    // user: userReducer,
     authentication: authReducer,
     content: contentReducer,
     favorites: favoritesReducer,
-    posts: postReducer,
+    posts: postsReducer,
     purchase: purchaseReducer,
-    messages: messagesReducer
+    messages: messagesReducer,
+    post: postReducer,
     // Add other reducers as needed
   },
 });
