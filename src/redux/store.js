1f045c942c03ab5eb0393ab5407c8c24707eeef4
LeslieAine <aineleslie@gmail.com>
2023-10-16 08:11:37 -0700

    Add contents reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import userReducer from './user/userSlice';
-import authReducer from './authentication/AuthenticationSlice';
+import authReducer from './authentication/AuthCreatorSlice';
 import contentReducer from './content/contentSlice'
 import favoritesReducer from './favorite/favoriteSlice'
 import postsReducer from './posts/postsSlice'
 import postReducer from './posts/postSlice'
 import purchaseReducer from './purchase/purchaseSlice';
 import messagesReducer from './messages/messagesSlice';
+import contentsSlice from './content/contentsSlice';
 
 const store = configureStore({
   reducer: {
     // user: userReducer,
     authentication: authReducer,
     content: contentReducer,
+    contents: contentsSlice,
     favorites: favoritesReducer,
     posts: postsReducer,
     purchase: purchaseReducer,
