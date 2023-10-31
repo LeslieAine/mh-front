30acc07336770595ffcc61c65757ebdd5a28bba3
LeslieAine <aineleslie@gmail.com>
2023-10-31 08:43:16 -0700

    update routes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -15,45 +15,25 @@ import CreatorPostList from '../components/creator/creator/CreatorPosts/CreatorP
 import samplePosts from '../components/Client/ClientHomePage/post/SamplePosts';
 import PostList from '../components/Client/ClientHomePage/post/PostList';
 import LandingPage from '../components/Pages/LandingPage/Landing';
+import Conversation from '../components/messages/ConversationPage';
+// import { APP_CABLE_URL } from './constants';
+import actionCable from 'actioncable';
 
 
 const AppRouter = () => {
-  const ProtectedRoute = ({ children }) => {
     const user = useSelector((state) => state.authentication.user) || JSON.parse(localStorage.getItem('user'));
+    const CableApp = {};
+    // CableApp.cable = actionCable.createConsumer(APP_CABLE_URL);
+    CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
+    // const userId = user.id;
 
+  const ProtectedRoute = ({ children }) => {
     if (!user) {
       return <Navigate to="/" replace />;
     }
 
     return children;
   };
-// const contentData = [
-//     {
-//       title: 'Song Title 1',
-//       numberOfBuys: 100,
-//       price: '$0.99',
-//       length: '3:45',
-//     },
-//     {
-//       title: 'Song Title 2',
-//       numberOfBuys: 85,
-//       price: '$1.25',
-//       length: '4:12',
-//     },
-//     {
-//       title: 'Podcast Episode 1',
-//       numberOfBuys: 50,
-//       price: '$0.00 (Free)',
-//       length: '25:30',
-//     },
-//     {
-//       title: 'Ebook - Introduction to React',
-//       numberOfBuys: 120,
-//       price: '$9.99',
-//       length: '150 pages',
-//     },
-//     // Add more content items as needed
-//   ];
 
 
   return (
@@ -63,12 +43,12 @@ const AppRouter = () => {
             <ProtectedRoute>
                 <CreatorHomePage />
             </ProtectedRoute>)}>
-            <Route path="posts" element={<CreatorPostList />}/>
+            <Route path="posts" element={<CreatorPostList user = {user}/>}/>
             <Route path="content" element={(<ContentPage />)}/>
             <Route path="orders" element={(<OrderPage />)}/>
-            <Route path="messages" element={(<DmPage />)}/>
+            <Route path="messages/*" element={(<Conversation cable={CableApp.cable} />)}/>
         </Route>
-        <Route path="/creator-profile" element={(
+        <Route path="/creator-profile/:id" element={(
             <ProtectedRoute>
                 <ViewCreator /> 
             </ProtectedRoute>)}>
