7bf1e46f0ce0ebee4e0f1a52e9f205aab5dbf0c0
LeslieAine <aineleslie@gmail.com>
2023-11-08 06:44:30 -0800

    Handle messages in chat rooms

---
+++ src/components/Chat/Messages.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Message from "./Message";
+import { ActionCableConsumer } from "react-actioncable-provider";
+
+export default function Messages({
+  messages,
+  receiveMessage,
+  user,
+  loadConversations,
+}){
+  const sortedMessages = messages.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+  const handleReceivedMessages = (data) => {
+    receiveMessage(data);
+    loadConversations(user.id);
+  };
+  const id = messages.length > 0 ? messages[0].conversation_id : 0;
+  return (
+    <>
+      <ActionCableConsumer
+        channel={{
+          channel: "ChatsChannel",
+          room_id: id,
+        }}
+        onReceived={handleReceivedMessages}
+      />
+      {sortedMessages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
+    </>
+  );
+}
