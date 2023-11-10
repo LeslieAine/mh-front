30a0e098b639b157e65678d9ca998df38c33e589
LeslieAine <aineleslie@gmail.com>
2023-11-10 11:56:37 -0800

    Update routes for chats

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import {
-  Routes, Route, Navigate
+  Routes, Route, Navigate, useParams
 } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import CreatorHomePage from '../components/creator/Pages/CreatorHomePage/CreatorHomePage';
@@ -15,9 +15,11 @@ import CreatorPostList from '../components/creator/creator/CreatorPosts/CreatorP
 import samplePosts from '../components/Client/ClientHomePage/post/SamplePosts';
 import PostList from '../components/Client/ClientHomePage/post/PostList';
 import LandingPage from '../components/Pages/LandingPage/Landing';
-import Conversation from '../components/messages/ConversationPage';
+// import Conversation from '../components/messages/ConversationPage';
 // import { APP_CABLE_URL } from './constants';
 import actionCable from 'actioncable';
+import DisplayChats from '../components/messages/DisplayChats';
+import Conversation from '../components/Room/Conversation';
 
 
 const AppRouter = () => {
@@ -26,6 +28,11 @@ const AppRouter = () => {
     // CableApp.cable = actionCable.createConsumer(APP_CABLE_URL);
     CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
     // const userId = user.id;
+
+//     const { id, conversationId } = useParams();
+
+//   console.log("User ID:", id);
+//   console.log("Conversation ID:", conversationId);
 
   const ProtectedRoute = ({ children }) => {
     if (!user) {
@@ -46,7 +53,18 @@ const AppRouter = () => {
             <Route path="posts" element={<CreatorPostList user = {user}/>}/>
             <Route path="content" element={(<ContentPage />)}/>
             <Route path="orders" element={(<OrderPage />)}/>
-            <Route path="messages/*" element={(<Conversation cable={CableApp.cable} />)}/>
+            {/* <Route path="messages/*" element={(<Conversation cable={CableApp.cable} />)}/> */}
+            <Route path="messages/*" element={(<DisplayChats cable={CableApp.cable}/>)}>
+                {/* <Route
+                // path={`/creator-homepage/messages/users/${userId}/conversations/${conversationId}`}
+                path="users/:id/conversations/:conversationId"
+                // path="/creator-homepage/messages/users/45/conversations/5"
+                // render={(routerProps) => <Conversation {...routerProps} />}
+                element={<Conversation />}
+                children={(routerProps) => <Conversation {...routerProps} />}
+                />  */}
+            </Route>
+
         </Route>
         <Route path="/creator-profile/:id" element={(
             <ProtectedRoute>
