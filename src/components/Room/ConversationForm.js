33c1751863819cda20c06282b35cbd8c202f36c6
LeslieAine <aineleslie@gmail.com>
2023-11-13 10:40:16 -0800

    Form to create chat(will be modified to be a click not form)

--- src/components/Room/ConversationForm.js
+++ src/components/Room/ConversationForm.js
@@ -10,7 +10,7 @@ const ConversationForm = () => {
   });
 
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.currentUser);
+  const user = useSelector((state) => state.authentication.user.status.data);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
