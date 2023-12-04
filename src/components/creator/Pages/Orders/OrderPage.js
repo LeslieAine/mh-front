3b84b60fdecc9edc24907d894d870532e7fb95ec
LeslieAine <aineleslie@gmail.com>
2023-12-04 07:23:01 -0800

    Add the order page as an outlet

--- src/components/creator/Pages/Orders/OrderPage.js
+++ src/components/creator/Pages/Orders/OrderPage.js
@@ -1,33 +1,23 @@
 import React from 'react'
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import OrderList from '../../creator/Orders/OrderList'
+import './OrderPage.css'
 
-function OrderPage() {
-    const orders = [
-        {
-          description: 'Design a website homepage',
-          requestedLength: '3 days',
-        },
-        {
-          description: 'Write a 1000-word article',
-          requestedLength: '2 days',
-        },
-        {
-          description: 'Create a logo for a company',
-          requestedLength: '1 week',
-        },
-        {
-          description: 'Translate a document to Spanish',
-          requestedLength: '1 day',
-        },
-        {
-          description: 'Develop a mobile app prototype',
-          requestedLength: '5 days',
-        },
-      ];
-      
+const OrderPage = () => {
+
+  const userId = useSelector((state) => state.authentication.user.status.data.id); 
+
+
   return (
     <div>
-        <OrderList orders = {orders} />
+        {/* <OrderList /> */}
+        <nav>
+        <Link className='nav-link' to={`/creator-homepage/orders/${userId}/made-orders`}>Made</Link>
+        <Link className='nav-link' to={`/creator-homepage/orders/${userId}/fulfilled-orders`}>Completed</Link>
+        <Link className='nav-link' to={`/creator-homepage/orders/${userId}/received-orders`}>Received</Link>
+      </nav>
+      <Outlet />
     </div>
   )
 }
